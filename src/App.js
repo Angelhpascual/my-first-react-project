7fd224385ab84cebd44442ed73cd62f14276ec8c
angelhpascual <angelhpascual@gmail.com>
2023-02-16 17:44:58 +0100

    My first react project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import SearchBar from "./components/SearchBar"
+import ProductTable from "./components/ProductTable"
+import { useState } from "react"
 
-function App() {
+export default function App({ products }) {
+  const [filterText, setFilterText] = useState("")
+  const [inStockOnly, setInStockOnly] = useState(false)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <SearchBar
+        onFilterTextChange={setFilterText}
+        onInStockOnlyChange={setInStockOnly}
+        filterText={filterText}
+        inStockOnly={inStockOnly}
+      />
+      <ProductTable
+        products={products}
+        filterText={filterText}
+        inStockOnly={inStockOnly}
+      />
     </div>
-  );
+  )
 }
-
-export default App;
